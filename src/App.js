96aa0d9649e4e687e2961d96ea34c0cdba023521
buckwheattN1 <grykajm@gmail.com>
2023-01-04 21:55:12 +0100

    add header compoment with basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Header from "./components/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./Header.module.css";
+
+function Header() {
+  return <header>LOOKS LIKE BIOS DOESN'T IT ?</header>;
+}
+
+export default Header;
