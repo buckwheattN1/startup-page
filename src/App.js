1b14b3905a7e0ed1775c063989a3c7d9f5d3e830
buckwheattN1 <grykajm@gmail.com>
2023-01-07 16:02:40 +0100

    Change content source file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 
+import Content from "./components/layout/Content";
 import Header from "./components/layout/Header";
-import MainPage from "./components/pages/Main";
-import SearchPage from "./components/pages/Search";
 
 function App() {
   return (
     <div>
       <Header />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
+      <Content />
     </div>
   );
 }
--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import MainPage from "../pages/Main";
+import SearchPage from "../pages/Search";
 
 function Content() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Content;
