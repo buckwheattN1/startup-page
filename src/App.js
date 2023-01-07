f437e520444bed0d58079906d34330989b53ec6b
buckwheattN1 <grykajm@gmail.com>
2023-01-07 11:58:55 +0100

    add routing to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Route, Routers, Routes } from "react-router-dom";
 
 import Header from "./components/layout/Header";
+import MainPage from "./components/pages/Main";
+import SearchPage from "./components/pages/Search";
 
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
     </div>
   );
 }
