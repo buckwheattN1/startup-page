af32e498eed066729b4976915350a6f5313c83fb
buckwheattN1 <grykajm@gmail.com>
2023-01-07 12:56:36 +0100

    remove incorrect import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routers, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Header from "./components/layout/Header";
 import MainPage from "./components/pages/Main";
