69671b2df9de932d4c0b70290af5fe506058daad
buckwheattN1 <grykajm@gmail.com>
2023-01-07 16:21:01 +0100

    styling changes

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -4,14 +4,18 @@ import { Route, Routes } from "react-router-dom";
 import MainPage from "../pages/Main";
 import SearchPage from "../pages/Search";
 
+import classes from "./Content.module.css";
+
 function Content() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
-    </div>
+    <main className={classes.main}>
+      <section className={classes.content}>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </section>
+    </main>
   );
 }
 
