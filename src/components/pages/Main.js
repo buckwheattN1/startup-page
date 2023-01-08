ae7c60d044d8b488db679c50fc8c2026caff8676
buckwheattN1 <grykajm@gmail.com>
2023-01-08 11:42:16 +0100

    set real-time clock and date

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function MainPage() {
-  return <div>TWOJA STARA - MAIN PAGEA</div>;
+  // Set time and refresh it every second
+  const [currentTime, setCurrentTime] = useState(
+    new Date().toLocaleTimeString("pl-PL", { hour12: false })
+  );
+
+  setInterval(() => {
+    setCurrentTime(new Date().toLocaleTimeString("pl-PL", { hour12: false }));
+  }, 1000);
+  // Date settings
+  const formatter = new Intl.DateTimeFormat("en-GB", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+  const date = new Date();
+  const result = formatter.format(date);
+
+  return (
+    <div>
+      <a className="nav">
+        System Date
+        <time style={{ margin: "4rem" }}>[{result}]</time>
+      </a>
+      <br />
+      <a className="nav">
+        System Time
+        <time style={{ margin: "4rem" }}>[{currentTime}]</time>
+      </a>
+    </div>
+  );
 }
 
 export default MainPage;
