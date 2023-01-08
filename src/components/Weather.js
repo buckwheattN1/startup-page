32280b07d931d620568b729b7e6bac6ead18b24f
buckwheattN1 <grykajm@gmail.com>
2023-01-08 14:01:29 +0100

    null

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Weather() {
   const api =
     "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/warsaw?unitGroup=metric&key=PKEKTDB4B4TMDVWFU85J7SXSW&contentType=json";
 
-  fetch(api, {
-    method: "GET",
-    headers: {},
-  })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  //   useEffect(() => {
+  //     fetchWeather();
+  //   }, []);
+
+  //   // Fetch Weather
+  //   const fetchWeather = async () => {
+  //     const res = await fetch(api);
+  //     const data = await res.json();
+
+  //     console.log(data.address);
+  //   };
+  //   return <div>{data.address}</div>;
 }
 
 export default Weather;
